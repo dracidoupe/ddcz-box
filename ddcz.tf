5928caf6d11848f1c038f65da2397248a5042539
Almad <bugs@almad.net>
2021-08-26 17:04:19 +0200

    Add cloudfront distribution

--- ddcz.tf
+++ ddcz.tf
@@ -10,13 +10,13 @@ terraform {
 provider "aws" {
   version = "~> 2.0"
   region  = "eu-central-1"
-  
 }
 
 locals {
-    internet_cidr = "0.0.0.0/0"
-    az = "eu-central-1b"
-    secondary_az = "eu-central-1a"
+  internet_cidr = "0.0.0.0/0"
+  az            = "eu-central-1b"
+  secondary_az  = "eu-central-1a"
+  user_uploads_domain = "uploady.dracidoupe.cz"
 }
 
 variable "RDS_PASSWORD" {}
@@ -25,9 +25,9 @@ resource "aws_ebs_volume" "ddcz_userdata_backup" {
   availability_zone = local.az
   size              = 1
   type              = "standard"
-  tags              = {
-      "Name" = "ddcz-userbackup"
-      "product" = "ddcz"
+  tags = {
+    "Name"    = "ddcz-userbackup"
+    "product" = "ddcz"
   }
 }
 
@@ -37,36 +37,36 @@ resource "aws_key_pair" "penpen" {
 }
 
 resource "aws_vpc" "ddcz_prod" {
-  cidr_block = "192.168.0.0/16"
-  instance_tenancy = "default"
-  enable_dns_support = "true"
+  cidr_block           = "192.168.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
   enable_dns_hostnames = "true"
 
-  tags              = {
-      "product" = "ddcz"
+  tags = {
+    "product" = "ddcz"
   }
 
 }
 
 resource "aws_subnet" "ddcz_prod" {
-  availability_zone = local.az
-  vpc_id     = aws_vpc.ddcz_prod.id
-  cidr_block = "192.168.1.0/24"
+  availability_zone       = local.az
+  vpc_id                  = aws_vpc.ddcz_prod.id
+  cidr_block              = "192.168.1.0/24"
   map_public_ip_on_launch = true
 
-  tags              = {
-      "product" = "ddcz"
+  tags = {
+    "product" = "ddcz"
   }
 }
 
 resource "aws_subnet" "ddcz_secondary_az" {
-  availability_zone = local.secondary_az
-  vpc_id     = aws_vpc.ddcz_prod.id
-  cidr_block = "192.168.2.0/24"
+  availability_zone       = local.secondary_az
+  vpc_id                  = aws_vpc.ddcz_prod.id
+  cidr_block              = "192.168.2.0/24"
   map_public_ip_on_launch = true
 
-  tags              = {
-      "product" = "ddcz"
+  tags = {
+    "product" = "ddcz"
   }
 
 }
@@ -75,20 +75,20 @@ resource "aws_subnet" "ddcz_secondary_az" {
 resource "aws_internet_gateway" "ddcz_prod" {
   vpc_id = aws_vpc.ddcz_prod.id
 
-  tags              = {
-      "product" = "ddcz"
+  tags = {
+    "product" = "ddcz"
   }
 }
 
 resource "aws_route_table" "ddcz_prod" {
- vpc_id = aws_vpc.ddcz_prod.id
- route {
+  vpc_id = aws_vpc.ddcz_prod.id
+  route {
     cidr_block = local.internet_cidr
     gateway_id = aws_internet_gateway.ddcz_prod.id
- }
- tags = {
-      "product" = "ddcz"
- }
+  }
+  tags = {
+    "product" = "ddcz"
+  }
 }
 resource "aws_route_table_association" "ddcz_prod" {
   subnet_id      = aws_subnet.ddcz_prod.id
@@ -96,66 +96,66 @@ resource "aws_route_table_association" "ddcz_prod" {
 }
 
 resource "aws_network_acl" "ddcz_prod" {
-  vpc_id = aws_vpc.ddcz_prod.id
-  subnet_ids = [ aws_subnet.ddcz_prod.id ]
+  vpc_id     = aws_vpc.ddcz_prod.id
+  subnet_ids = [aws_subnet.ddcz_prod.id]
 
   ingress {
-      protocol = "all"
-      rule_no = 100
-      action = "allow"
-      cidr_block = local.internet_cidr
-      from_port = 0
-      to_port = 0
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = local.internet_cidr
+    from_port  = 0
+    to_port    = 0
   }
 
   egress {
-      protocol = "all"
-      rule_no = 100
-      action = "allow"
-      cidr_block = local.internet_cidr
-      from_port = 0
-      to_port = 0
+    protocol   = "all"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = local.internet_cidr
+    from_port  = 0
+    to_port    = 0
   }
 
-#   ingress {
-#     protocol   = "tcp"
-#     rule_no    = 100
-#     action     = "allow"
-#     cidr_block = local.internet_cidr
-#     from_port  = 22
-#     to_port    = 22
-#   }
-  
-#   ingress {
-#     protocol   = "tcp"
-#     rule_no    = 200
-#     action     = "allow"
-#     cidr_block = local.internet_cidr
-#     from_port  = 80
-#     to_port    = 80
-#   }
-  
-#   egress {
-#     protocol   = "tcp"
-#     rule_no    = 100
-#     action     = "allow"
-#     cidr_block = local.internet_cidr
-#     from_port  = 22 
-#     to_port    = 22
-#   }
-  
-#   egress {
-#     protocol   = "tcp"
-#     rule_no    = 200
-#     action     = "allow"
-#     cidr_block = local.internet_cidr
-#     from_port  = 80  
-#     to_port    = 80 
-#   }
- 
-   tags = {
+  #   ingress {
+  #     protocol   = "tcp"
+  #     rule_no    = 100
+  #     action     = "allow"
+  #     cidr_block = local.internet_cidr
+  #     from_port  = 22
+  #     to_port    = 22
+  #   }
+
+  #   ingress {
+  #     protocol   = "tcp"
+  #     rule_no    = 200
+  #     action     = "allow"
+  #     cidr_block = local.internet_cidr
+  #     from_port  = 80
+  #     to_port    = 80
+  #   }
+
+  #   egress {
+  #     protocol   = "tcp"
+  #     rule_no    = 100
+  #     action     = "allow"
+  #     cidr_block = local.internet_cidr
+  #     from_port  = 22 
+  #     to_port    = 22
+  #   }
+
+  #   egress {
+  #     protocol   = "tcp"
+  #     rule_no    = 200
+  #     action     = "allow"
+  #     cidr_block = local.internet_cidr
+  #     from_port  = 80  
+  #     to_port    = 80 
+  #   }
+
+  tags = {
     "product" = "ddcz"
-   }
+  }
 }
 
 resource "aws_security_group" "ddcz" {
@@ -194,44 +194,45 @@ resource "aws_security_group" "ddcz" {
     cidr_blocks = [local.internet_cidr]
   }
 
-  tags              = {
-      "product" = "ddcz"
+  tags = {
+    "product" = "ddcz"
   }
 
 }
 
 resource "aws_db_subnet_group" "ddcz_mysql" {
-    name = "ddcz-mysql-subnet"
-    description = "RDS subnet group"
-    subnet_ids = [aws_subnet.ddcz_prod.id, aws_subnet.ddcz_secondary_az.id]
+  name        = "ddcz-mysql-subnet"
+  description = "RDS subnet group"
+  subnet_ids  = [aws_subnet.ddcz_prod.id, aws_subnet.ddcz_secondary_az.id]
 }
 
 
 resource "aws_db_instance" "mysql" {
-    availability_zone = local.az
-    allocated_storage = 20
-    engine = "mysql"
-    engine_version = "5.7"
-    instance_class = "db.t3.micro"
-    identifier = "ddcz-mysql"
-    name = "dracidoupe_cz"
-    username = "root"
-    password = var.RDS_PASSWORD
-    parameter_group_name = "default.mysql5.7"
-    skip_final_snapshot = true
-    final_snapshot_identifier = "ddcz-mysql-snap"
+  availability_zone         = local.az
+  allocated_storage         = 20
+  engine                    = "mysql"
+  engine_version            = "5.7"
+  instance_class            = "db.t3.micro"
+  identifier                = "ddcz-mysql"
+  name                      = "dracidoupe_cz"
+  username                  = "root"
+  password                  = var.RDS_PASSWORD
+  parameter_group_name      = "default.mysql5.7"
+  skip_final_snapshot       = true
+  final_snapshot_identifier = "ddcz-mysql-snap"
+  deletion_protection = true
 
-    multi_az = "false"
+  multi_az = "false"
 
-    db_subnet_group_name = aws_db_subnet_group.ddcz_mysql.name
-    vpc_security_group_ids = [aws_security_group.ddcz.id]
+  db_subnet_group_name   = aws_db_subnet_group.ddcz_mysql.name
+  vpc_security_group_ids = [aws_security_group.ddcz.id]
 
-    storage_type = "standard"
-    publicly_accessible = "true"
+  storage_type        = "standard"
+  publicly_accessible = "true"
 
-    tags = {
-        "product" = "ddcz"
-    }
+  tags = {
+    "product" = "ddcz"
+  }
 }
 
 resource "aws_s3_bucket" "ddcz_uploads_bucket" {
@@ -245,25 +246,25 @@ resource "aws_s3_bucket" "ddcz_uploads_bucket" {
 
 
 resource "aws_eip" "ddcz" {
-  instance = aws_instance.ddcz.id
-  vpc      = true
+  instance   = aws_instance.ddcz.id
+  vpc        = true
   depends_on = [aws_internet_gateway.ddcz_prod, aws_instance.ddcz]
 }
 
 resource "aws_instance" "ddcz" {
-  ami           = "ami-041855a8b7934ebae"
-  instance_type = "t2.nano"
+  ami                     = "ami-041855a8b7934ebae"
+  instance_type           = "t2.nano"
   disable_api_termination = "true"
-  key_name      = aws_key_pair.penpen.key_name
-  availability_zone = local.az
+  key_name                = aws_key_pair.penpen.key_name
+  availability_zone       = local.az
 
   associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.ddcz.id]
-  subnet_id              = aws_subnet.ddcz_prod.id
+  vpc_security_group_ids      = [aws_security_group.ddcz.id]
+  subnet_id                   = aws_subnet.ddcz_prod.id
 
   root_block_device {
-    volume_type = "standard"
-    volume_size = 4
+    volume_type           = "standard"
+    volume_size           = 4
     delete_on_termination = true
   }
 
@@ -274,9 +275,9 @@ resource "aws_instance" "ddcz" {
     private_key = file("~/.ssh/aws-penpen.pem")
     host        = self.public_ip
   }
-  
-#  user_data = file("setup.sh")
-  
+
+  #  user_data = file("setup.sh")
+
   provisioner "file" {
     source      = "etc/services/run"
     destination = "/etc/service/dracidoupe.cz/run"
@@ -292,45 +293,45 @@ resource "aws_instance" "ddcz" {
     destination = "/var/www/dracidoupe.cz/www_root/www/htdocs/dbcore.php"
   }
 
-#   provisioner "file" {
-#     source      = "etc/modules/custom-access-log"
-#     destination = "/etc/lighttpd/modules/custom-access-log"
-#   }
+  #   provisioner "file" {
+  #     source      = "etc/modules/custom-access-log"
+  #     destination = "/etc/lighttpd/modules/custom-access-log"
+  #   }
 
-#   provisioner "file" {
-#     source      = "etc/modules/fcgi-socket-php"
-#     destination = "/etc/lighttpd/modules/fcgi-socket-php"
-#   }
+  #   provisioner "file" {
+  #     source      = "etc/modules/fcgi-socket-php"
+  #     destination = "/etc/lighttpd/modules/fcgi-socket-php"
+  #   }
 
-#   provisioner "file" {
-#     source      = "etc/sites/dracidoupe.cz"
-#     destination = "/etc/lighttpd/sites/dracidoupe.cz"
-#   }
+  #   provisioner "file" {
+  #     source      = "etc/sites/dracidoupe.cz"
+  #     destination = "/etc/lighttpd/sites/dracidoupe.cz"
+  #   }
 
-  tags              = {
-      "product" = "ddcz"
+  tags = {
+    "product" = "ddcz"
   }
 
 
-#   provisioner "remote-exec" {
-#     inline = [
-#         "mkdir /var/www",
-#         "mount -t ext4 /dev/xvdf1 /var/www",
-#         "sudo echo 'deb http://archive.debian.org/debian squeeze main' > /etc/apt/sources.list'",
-#         "sudo echo 'deb http://archive.debian.org/debian squeeze-lts main' >> /etc/apt/sources.list",
-#         "sudo echo 'Acquire::Check-Valid-Until false;' > /etc/apt/apt.conf",
-#         "sudo apt-get update",
-#         "sudo apt-get -y --force-yes -q install lighttpd php5-cgi php5-cli php5-curl php5-imagick php5-mysql daemontools daemontools-run procps spawn-fcgi",
-#         "groupadd w-dracidoupe-cz",
-#         "useradd w-dracidoupe-cz -g w-dracidoupe-cz",
-#         "groupadd wwwserver",
-#         "useradd lighttpd -g www-data -g wwwserver",
-#         "mkdir /etc/service/dracidoupe.cz",
-#         "mkdir -p /var/www/dracidoupe.cz/www_root/www/php/",
-#         "mkdir -p /var/www/fastcgi/sockets/w-dracidoupe-cz/",
-#         "chown -R w-dracidoupe-cz:wwwserver /var/www/fastcgi/sockets/w-dracidoupe-cz/",
-#     ]
-#   }
+  #   provisioner "remote-exec" {
+  #     inline = [
+  #         "mkdir /var/www",
+  #         "mount -t ext4 /dev/xvdf1 /var/www",
+  #         "sudo echo 'deb http://archive.debian.org/debian squeeze main' > /etc/apt/sources.list'",
+  #         "sudo echo 'deb http://archive.debian.org/debian squeeze-lts main' >> /etc/apt/sources.list",
+  #         "sudo echo 'Acquire::Check-Valid-Until false;' > /etc/apt/apt.conf",
+  #         "sudo apt-get update",
+  #         "sudo apt-get -y --force-yes -q install lighttpd php5-cgi php5-cli php5-curl php5-imagick php5-mysql daemontools daemontools-run procps spawn-fcgi",
+  #         "groupadd w-dracidoupe-cz",
+  #         "useradd w-dracidoupe-cz -g w-dracidoupe-cz",
+  #         "groupadd wwwserver",
+  #         "useradd lighttpd -g www-data -g wwwserver",
+  #         "mkdir /etc/service/dracidoupe.cz",
+  #         "mkdir -p /var/www/dracidoupe.cz/www_root/www/php/",
+  #         "mkdir -p /var/www/fastcgi/sockets/w-dracidoupe-cz/",
+  #         "chown -R w-dracidoupe-cz:wwwserver /var/www/fastcgi/sockets/w-dracidoupe-cz/",
+  #     ]
+  #   }
 }
 
 resource "aws_volume_attachment" "ebs_att" {
@@ -338,4 +339,82 @@ resource "aws_volume_attachment" "ebs_att" {
   volume_id    = aws_ebs_volume.ddcz_userdata_backup.id
   instance_id  = aws_instance.ddcz.id
   skip_destroy = true
+}
+
+# resource "aws_acm_certificate" "certificate" {
+#   domain_name       = local.user_uploads_domain]
+#   validation_method = "EMAIL"
+
+#   # subject_alternative_names = ["dracidoupe.cz"]
+# }
+
+resource "aws_cloudfront_distribution" "s3_ddcz_uploads_dist" {
+  origin {
+    # domain_name = "${aws_s3_bucket.www.website_endpoint}"
+    domain_name = aws_s3_bucket.ddcz_uploads_bucket.bucket_regional_domain_name
+    origin_id   = "ddcz_uploads_origin"
+  }
+  enabled         = true
+  is_ipv6_enabled = true
+  default_root_object = "index.html"
+  price_class = "PriceClass_100"
+
+  # aliases = [local.user_uploads_domain]
+
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "ddcz_uploads_origin"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 864000
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "ddcz_uploads_origin"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  // This is we'd like to switch to ACM/Let's Encrypt
+  # viewer_certificate {
+  #   acm_certificate_arn = "${aws_acm_certificate.certificate.arn}"
+  #   ssl_support_method  = "sni-only"
+  # }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    "product" = "ddcz"
+  }
+
+
 }
